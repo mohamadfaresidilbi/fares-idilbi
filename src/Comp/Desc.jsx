841808f47eb0e8011373c93561cdbbc2d274adf1
mohamadfaresidilbi <91882512+mohamadfaresidilbi@users.noreply.github.com>
2021-12-28 17:59:52 +0200

    remove cv

--- src/Comp/Desc.jsx
+++ src/Comp/Desc.jsx
@@ -21,7 +21,7 @@ function Desc() {
                             <a href="https://www.google.com/maps/place/Beirut,+Lebanon/@33.8892183,35.5042823,13z/data=!4m2!3m1!1s0x151f17215880a78f:0x729182bae99836b4" target="_blank"><i class="fas fa-map-pin"></i> &nbsp;<b>Beirut, Lebanon</b></a>
                             <a href="tel:+96176732957"><i class="fas fa-phone"></i> &nbsp;<b>+961 76732957</b></a>
                             <a href="mailto:mhdfaresidilbi@gmail.com"><i class="fas fa-envelope"></i> &nbsp;<b>mhdfaresidilbi@gmail.com</b></a>
-                            <a href="https://www.faresidilbi.com"><i class="fas fa-paperclip"></i> &nbsp;<b>faresidilbi.com</b></a>
+                            <a href="https://mohamadfaresidilbi.github.io/myWebsite/"><i class="fas fa-paperclip"></i> &nbsp;<b>https://mohamadfaresidilbi.github.io/myWebsite/</b></a>
                             <a href={files} target="_blank" class="cv" download="Fares Idilbi - CV"><i class="fas fa-file-export"></i> &nbsp;<b>My CV</b></a>
                     </div>
                 </div>
