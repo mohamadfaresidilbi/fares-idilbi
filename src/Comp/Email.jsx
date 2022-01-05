43ce76761809ffb38204de7f32acd0b303eedf36
mohamadfaresidilbi <91882512+mohamadfaresidilbi@users.noreply.github.com>
2022-01-05 23:31:37 +0200

    media fixes

--- src/Comp/Email.jsx
+++ src/Comp/Email.jsx
@@ -8,11 +8,11 @@ function Email() {
                 <h3>Contact Me</h3>
                 <div>
                     <form action="mailto:mhdfaresidilbi@gmail.com" method="POST" enctype="text/plain">            
-                        <input type="text" id="fname" name="First name " placeholder="First Name" required="required"/>
-                        <input type="text" id="lname" name="Last name " placeholder="Last Name" required="required"/><br/>
-                        <input type="email" id="email" name="Email " placeholder="Email" required="required" />
-                        <input type="tel" id="tel" name="Phone number " placeholder="76732957" required="required" /> <br/>
-                        <textarea id="msg" name="Message " rows="4" cols="50" placeholder="Your Message" required="required" />
+                        <input type="text" id="fname" name="First name " placeholder="First Name *" required="required"/>
+                        <input type="text" id="lname" name="Last name " placeholder="Last Name *" required="required"/><br/>
+                        <input type="email" id="email" name="Email " placeholder="Email *" required="required" />
+                        <input type="tel" id="tel" name="Phone number " placeholder="+961 76727376" /> <br/>
+                        <textarea id="msg" name="Message " rows="4" cols="50" placeholder="Your Message *" required="required" />
                         <input type="submit" value="Submit" />
                     </form>
                 </div>
--- src/Comp/Volunteer.jsx
+++ src/Comp/Volunteer.jsx
@@ -7,8 +7,8 @@ function Volunteer() {
                 <h3>Volunteering</h3>
                 <div>
                     <ul>
+                    <li><p id="title-pad"><u>Foodblessed</u></p><p><span id="greys">2019 - 2021</span></p></li>
                         <li><p id="title-pad"><u>Ajialouna Organization</u></p><p><span id="greys">2017</span></p></li>
-                        <li><p id="title-pad"><u>Foodblessed</u></p><p><span id="greys">2019 - 2021</span></p></li>
                     </ul>
                 </div>
             </section>
